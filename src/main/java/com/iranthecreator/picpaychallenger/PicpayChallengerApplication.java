a6337f6eb1fe70f923bfce582968051f13ae7144
Iranildo Alves <117860354+IranTheCreator@users.noreply.github.com>
2024-03-12 13:01:46 -0300

    feat: Database Configuration

---
+++ src/main/java/com/iranthecreator/picpaychallenger/PicpayChallengerApplication.java
@@ -0,0 +1,13 @@
+package com.iranthecreator.picpaychallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayChallengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iranthecreator/picpaychallenger/PicpayChallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iranthecreator.picpaychallenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayChallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
