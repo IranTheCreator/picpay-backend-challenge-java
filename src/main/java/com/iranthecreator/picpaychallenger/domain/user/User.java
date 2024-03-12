104ce4dd22832eb29d37e82ce3845e3929a8af66
Iranildo Alves <117860354+IranTheCreator@users.noreply.github.com>
2024-03-12 12:58:00 -0300

    feat: Create User Entity

---
+++ src/main/java/com/iranthecreator/picpaychallenger/domain/user/User.java
@@ -0,0 +1,38 @@
+package com.iranthecreator.picpaychallenger.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String LastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+}
---
+++ src/main/java/com/iranthecreator/picpaychallenger/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.iranthecreator.picpaychallenger.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
